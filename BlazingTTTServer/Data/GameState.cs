6eea4c7638cf1cfb8877b9aa5815ca62037a5863
Yaolin Ge <geyaolin@gmail.com>
2023-11-13 17:48:42 +0100

    update tictactoe

---
+++ BlazingTTTServer/Data/GameState.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazingTTTServer.Data
+{
+    public class GameState
+    {
+        public char[,] Board { get; set; } = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+        public char Player { get; set; } = 'o';
+        public bool IsGameOver { get; set; } = false;
+        public char Winner { get; set; } = ' ';
+        public bool IsDraw { get; set; } = false;
+    }
+}
---
+++ notes4.md
@@ -0,0 +1,9 @@
+ActivePageProvider:
+- Add navmenu items
+
+```{cs}
+<Tile CssClass="grs-1 gcs-1 gre-end gre-end">
+Some code here!
+</Tile>
+```
+-rem and font-size
